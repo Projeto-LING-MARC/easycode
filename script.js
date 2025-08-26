574a27b380f0d41be2b600adb3d044a131df38f8
Lucas Pereira <lucaspereiraprooficial@gmail.com>
2025-08-26 20:48:41 -0300

    Estruturando e estilizando a página

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.getElementById('Button_Editor').addEventListener('click', () => {
     window.location.href = "editor/editor.html";
 });
+
+// Botão do header
+document.getElementById('btn-destaque_header').addEventListener('click', () => {
+    window.location.href = "comeceaaprender/index.html";
+});
+
+// Botão da landing page
+document.getElementById('btn-landing-comecar').addEventListener('click', () => {
+    window.location.href = "comeceaaprender/index.html";
+});
